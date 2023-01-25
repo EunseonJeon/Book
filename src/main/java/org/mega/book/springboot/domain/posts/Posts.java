88045fc1690b5f355afcfd831666b7acad78e824
EunseonJeon <jes0213@gmail.com>
2023-01-25 17:45:43 +0900

    20230125_소스코드 업데이트

---
+++ src/main/java/org/mega/book/springboot/domain/posts/Posts.java
@@ -0,0 +1,36 @@
+package org.mega.book.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor  // 기본 생성자 자동 추가
+@Entity
+public class Posts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder    // @Builder를 통해 제공되는 빌드 클래스를 사용, 생성자나 빌더 생성 역할은 같음
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
